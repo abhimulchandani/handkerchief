d12df3eda75dc050c4e00c0670f24b041ec5631a
Abhishek Mulchandani <42692137+abhimulchandani@users.noreply.github.com>
2020-08-10 04:48:53 +0530

    Update db.php

--- db.php
+++ db.php
@@ -5,13 +5,6 @@ $dbuser = "abhi";
 $dbpass = "abhi";
 $db = "handkerchief";
 
-$loginUrl = "login.php";
-$logoutUrl = "logout.php";
-$indexUrl = "index.php";
-$moviepageUrl = "moviepage.php";
-$screenpageUrl = "screenpage.php";
-$bookedticketsUrl = "bookedtickets.php";
-
 $conn = mysqli_connect($host, $dbuser, $dbpass, $db);
 
 if(mysqli_connect_errno($conn)) {
