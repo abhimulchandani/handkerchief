00d678b2c6cf29361899cfce04632c43aaad5eee
Abhishek Mulchandani <42692137+abhimulchandani@users.noreply.github.com>
2020-09-01 12:37:35 +0530

    Update ratemovie.php

--- ratemovie.php
+++ ratemovie.php
@@ -13,6 +13,25 @@ if(!mysqli_query($conn, $sql)) {
 	echo "<p>ERROR: Failed to execute query $sql ".mysqli_error($conn)."</p>";
 }
 else {
-	echo "<p>Your rating: <span id='userrating'> $rating%</span></p>";
+
+	//Extracting average movie rating from the table movie_rating
+	$sql = "SELECT AVG(rating) AS avg, COUNT(rating) AS votes FROM movie_rating WHERE movie_id=$movieId ";
+	if($result = mysqli_query($conn, $sql)) {
+	    $row = mysqli_fetch_assoc($result);
+	    
+	    $rating = round($row['avg']);
+	    $number_of_votes = $row['votes'];
+	}
+	else {
+	    echo "<p>ERROR: Failed to execute query $sql ".mysqli_error($conn)."</p>";
+	}
+
+	//Inserting the computed average movie rating into the table movie
+	$sql = "UPDATE movie SET rating=$rating WHERE movie_id=$movieId";
+	if(!mysqli_query($conn, $sql)) {
+	    echo "<p>ERROR: Failed to execute query $sql ".mysqli_error($conn)."</p>";
+	}
+
+	echo "<p><span id='heart'>&#9829;</span> <span id='value'> $rating% </span> &nbsp;($number_of_votes votes)</p><div id='slider-container'><p>Your rating: <span id='userrating'> $rating%</span></p>";
 }
 ?>
